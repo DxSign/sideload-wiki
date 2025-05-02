be999a2b7a2f9e85925fb326ac3aaf657687a5a0
9z91 <root@9z91.dev>
2025-05-02 14:25:35 +0000

    Deploying to gh-pages from @ DxSign/sideload-wiki@c1a30e62ee1eda01e273d5a52d89e6c71e89c8a7 🚀

--- assets/install-Di31FESY.js
+++
@@ -1,1 +0,0 @@
-const s="/images/feather/choosefile.png",a="/images/feather/uploadandsign.png",e="/images/feather/install.png";export{s as _,a,e as b};
---
+++ assets/install-m5t1CwdT.js
@@ -0,0 +1,1 @@
+const i="/sideload-wiki/images/feather/choosefile.png",s="/sideload-wiki/images/feather/uploadandsign.png",a="/sideload-wiki/images/feather/install.png";export{i as _,s as a,a as b};
